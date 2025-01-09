10c59e39881914eed42a1013bb4344232c915a46
noobkyle <kadangotinaishe5@gmail.com>
2025-01-09 17:49:29 -0600

    accounts page complete

--- tripherweb/src/pages/Tab1.tsx
+++ tripherweb/src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { add, colorPalette, globe, refresh } from 'ionicons/icons';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -7,16 +8,40 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Tripher Banking App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Tripher Banking App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Checking Account</IonCardSubtitle>
+            <IonCardTitle>$3,800.00</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>read more</IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Savings Account </IonCardSubtitle>
+            <IonCardTitle>$77,456.88</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>read more</IonCardContent>
+        </IonCard>
+
+
+        <IonFab slot="fixed" vertical="bottom" horizontal="end">
+          <IonFabButton>
+            <IonIcon icon={refresh}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
       </IonContent>
     </IonPage>
   );
